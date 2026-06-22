213de785893a32458141924fda916a0de4a60fc4
Linda Mbongue <linda.mbongue@outlook.fr>
2026-06-22 16:18:13 +0000

    Class PreApproch declaration with methods + Build Configuration done

---
+++ include/pre_approach.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription.hpp"
+
+class PreApproach : public rclcpp::Node() {
+
+public:
+  PreApproach(const std::string &node_name);
+
+  ~PreApproach() = default;
+
+private:
+  std::string node_name_;
+
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_unstamped_pub;
+
+  void laser_scan_clbk(const sensor_msgs::msg::LaserScan::SharedPtr &msg);
+};
