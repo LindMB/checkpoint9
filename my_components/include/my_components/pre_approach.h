c76ce1b5a10c4f76203d7dbf0bf161ecec85854f
Linda Mbongue <linda.mbongue@outlook.fr>
2026-06-28 16:41:25 +0000

    Convert PreApproach class into a ROS Component

---
+++ my_components/include/my_components/pre_approach.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/node_options.hpp"
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription.hpp"
+#include "rclcpp/timer.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+namespace my_components {
+
+class PreApproach : public rclcpp::Node {
+
+public:
+  PreApproach(const rclcpp::NodeOptions &options);
+
+  void stop_robot();
+  bool is_pre_approach_completed() const;
+
+  ~PreApproach() = default;
+
+private:
+  double obstacle_; // Distance from the wall (in m)
+  int degrees_;     // Degrees to rotate the robot after stopping
+
+  double previous_yaw_;
+  double accumulated_yaw_ = 0.0;
+  double linear_x_vel = 0.2;
+  double angular_z_vel = 0.4;
+
+  bool first_odom_ = true;
+  bool obstacle_detected_ = false;
+  bool rotation_completed_ = false;
+  bool pre_approach_completed_ = false;
+
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr
+      cmd_vel_unstamped_pub_;
+  rclcpp::TimerBase::SharedPtr cmd_vel_unstamped_pub_timer_;
+
+  void laser_scan_clbk_(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void odom_callback_(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void cmd_vel_unstamped_pub_timer_clbk_();
+
+  void move_forward_();
+  void rotate_of_x_degrees_();
+  bool is_obstacle_detected_at_x_meters_(
+      const sensor_msgs::msg::LaserScan::SharedPtr msg);
+};
+
+} // namespace my_components
---
+++ my_components/src/pre_approach.cpp
@@ -0,0 +1,214 @@
+#include "my_components/pre_approach.h"
+#include "rclcpp/create_publisher.hpp"
+#include "rclcpp/create_subscription.hpp"
+#include "rclcpp/create_timer.hpp"
+#include "rclcpp/node_options.hpp"
+#include "rclcpp/qos.hpp"
+#include "rclcpp/utilities.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+#include <cmath>
+#include <memory>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+namespace my_components {
+
+PreApproach::PreApproach(const rclcpp::NodeOptions &options)
+    : Node("preapproach", options) {
+
+  this->obstacle_ = 0.3;
+  this->degrees_ = -90;
+
+  auto qos = rclcpp::QoS(10).reliability(rclcpp::ReliabilityPolicy::Reliable);
+
+  this->laser_scan_sub_ =
+      this->create_subscription<sensor_msgs::msg::LaserScan>(
+          "/scan", qos,
+          std::bind(&PreApproach::laser_scan_clbk_, this,
+                    std::placeholders::_1));
+
+  this->odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/odom", qos,
+      std::bind(&PreApproach::odom_callback_, this, std::placeholders::_1));
+
+  this->cmd_vel_unstamped_pub_ =
+      this->create_publisher<geometry_msgs::msg::Twist>(
+          "/diffbot_base_controller/cmd_vel_unstamped", 10);
+
+  auto timer_period = std::chrono::milliseconds(100);
+  this->cmd_vel_unstamped_pub_timer_ = this->create_wall_timer(
+      timer_period,
+      std::bind(&PreApproach::cmd_vel_unstamped_pub_timer_clbk_, this));
+
+  RCLCPP_INFO(this->get_logger(),
+              "PreApproach initialised | obstacle=%.2f degrees=%d", obstacle_,
+              degrees_);
+}
+
+void PreApproach::odom_callback_(const nav_msgs::msg::Odometry::SharedPtr msg) {
+
+  // Retrieve robot orientation from msg
+  tf2::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
+                    msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+
+  // Convert quaternion to angle (roll, pitch, yaw)
+  // with yaw -> orientation around the z-axis
+  tf2::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  // For the first odom msg, just init previous_yam_
+  if (first_odom_) {
+    this->previous_yaw_ = yaw;
+    this->first_odom_ = false;
+    return;
+  }
+
+  // Calculate the variation angle around the z-axis
+  double delta_yaw = yaw - this->previous_yaw_;
+
+  // Normalize the delta_yaw to the range [-pi, pi]
+  if (delta_yaw > M_PI) {
+    delta_yaw -= 2.0 * M_PI;
+  } else if (delta_yaw < -M_PI) {
+    delta_yaw += 2.0 * M_PI;
+  }
+
+  // If the robot is currently rotating of x degrees after reaching the wall
+  if (this->obstacle_detected_ && !this->rotation_completed_) {
+
+    this->accumulated_yaw_ += std::abs(delta_yaw);
+
+    RCLCPP_INFO(this->get_logger(), "accumulated_yaw_ : %.2f",
+                this->accumulated_yaw_);
+  }
+
+  // Update previous_yaw for the next calculation
+  this->previous_yaw_ = yaw;
+}
+
+void PreApproach::laser_scan_clbk_(
+    const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+  // If no obstacle detected yet
+  if (!this->obstacle_detected_) {
+    if (is_obstacle_detected_at_x_meters_(msg)) {
+      this->obstacle_detected_ = true;
+    } else {
+      // Do nothing
+    }
+  } else {
+    // Do nothing
+  }
+}
+
+void PreApproach::move_forward_() {
+
+  auto move_forward = geometry_msgs::msg::Twist();
+  move_forward.linear.x = this->linear_x_vel;
+  move_forward.angular.z = 0.0;
+
+  cmd_vel_unstamped_pub_->publish(move_forward);
+}
+
+void PreApproach::stop_robot() {
+
+  auto stop_msg = geometry_msgs::msg::Twist();
+
+  cmd_vel_unstamped_pub_->publish(stop_msg);
+}
+
+bool PreApproach::is_obstacle_detected_at_x_meters_(
+    const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+  double angle_rad;
+
+  for (size_t i = 0; i < msg->ranges.size(); i++) {
+
+    angle_rad = msg->angle_min + (i * msg->angle_increment);
+
+    // Front section -> Between -30deg and 30deg
+    bool is_angle_in_front_section =
+        (angle_rad >= -(M_PI / 6) && angle_rad <= M_PI / 6);
+
+    // If the ray distance is different from inf, -inf and NAN
+    // and is in the front section
+    if (std::isfinite(msg->ranges[i]) && is_angle_in_front_section) {
+
+      // If obstacle detected
+      if (msg->ranges[i] <= this->obstacle_) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+void PreApproach::rotate_of_x_degrees_() {
+
+  // Convert angle in degrees into radians
+  double angle_rad = (M_PI * this->degrees_) / 180;
+
+  // Init rotation message to publish
+  auto rotate_msg = geometry_msgs::msg::Twist();
+  rotate_msg.linear.x = 0.0;
+
+  // Define the rotation direction based on the sign of angle_rad
+  if (angle_rad < 0) {
+    rotate_msg.angular.z = -this->angular_z_vel;
+  } else {
+    rotate_msg.angular.z = this->angular_z_vel;
+  }
+
+  // If the robot finished rotating of x degrees
+  if (this->accumulated_yaw_ >= std::abs(angle_rad)) {
+
+    // Stop the robot
+    stop_robot();
+
+    this->rotation_completed_ = true;
+    RCLCPP_INFO(this->get_logger(), "Rotation completed !");
+
+    this->pre_approach_completed_ = true;
+    RCLCPP_INFO(this->get_logger(), "Pre-Approach completed !");
+
+    // Prepare robot for next rotation
+    this->accumulated_yaw_ = 0.0;
+
+  } else { // Continue to rotate
+    cmd_vel_unstamped_pub_->publish(rotate_msg);
+  }
+}
+
+void PreApproach::cmd_vel_unstamped_pub_timer_clbk_() {
+
+  if (!this->pre_approach_completed_) {
+
+    /// If the wall has been detected wall at x meters
+    if (this->obstacle_detected_) {
+
+      /// Stop
+      stop_robot();
+
+      /// Rotate of x degrees
+      rotate_of_x_degrees_();
+
+    } else {
+      /// Move forward
+      move_forward_();
+    }
+
+  } else {
+    // Do nothing
+  }
+}
+
+bool PreApproach::is_pre_approach_completed() const {
+  return this->pre_approach_completed_;
+}
+
+} // namespace my_components
+
+// Register my class as an Component
+RCLCPP_COMPONENTS_REGISTER_NODE(my_components::PreApproach)
